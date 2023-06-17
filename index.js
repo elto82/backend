07cb58874ba18264492d2a8b143039e828107770
Argiro <elto.82@gmai.com>
2023-06-16 23:08:00 -0500

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: "http://127.0.0.1:5173",
+    origin: "https://frontend-chat-socketio-nodejs.vercel.app/",
   },
 });
 
